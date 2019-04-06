f87b777ae6a23ef22210777983bd632b9438ad59
SamGinzburg <ginzburg.sam@gmail.com>
2019-04-06 16:32:40 -0400

    shared lib

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,11 +1,15 @@
 extern crate clap;
+extern crate sharedlib;
 
 use clap::{App};
+use sharedlib::example_fn;
 
 fn main() {
     App::new("Vuvuzela Client")
          .version("1.0")
          .about("Vuvuzela Client")
          .author("Sam Ginzburg")
+         .author("Benjamin Kuykendall")
          .get_matches();
+    example_fn();
 }
