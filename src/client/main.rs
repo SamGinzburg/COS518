b13179241f5ea516b533994337bcd96e34619e7d
SamGinzburg <ginzburg.sam@gmail.com>
2019-04-16 19:08:51 -0400

    scrollbar working

--- src/client/main.rs
+++ src/client/main.rs
@@ -9,6 +9,16 @@ use cursive::views::*;
 use cursive::event::*;
 
 fn send_message(s: &mut Cursive, message: &str) {
+    let mut text_area: ViewRef<TextView> = s.find_id("output").unwrap();
+    let mut text_input: ViewRef<EditView> = s.find_id("input").unwrap();
+
+    // clear the input
+    text_input.set_content("");
+
+    let mut input: String = "".to_string();
+    input.push_str(&message.to_string());
+    input.push_str("\n");
+    text_area.append(input);
 }
 
 fn main() {
@@ -22,7 +32,7 @@ fn main() {
     // set up main TUI context
     let mut cursive = Cursive::default();
 
-    // 
+    //
     // Create a view tree with a TextArea for input, and a
     // TextView for output.
     //
@@ -34,12 +44,15 @@ fn main() {
     let text_box_view = BoxView::new(SizeConstraint::Full, SizeConstraint::Free,
                                      Panel::new(text_area));
 
+    let mut scrollbar = ScrollView::new(TextView::new("")
+                                    .with_id("output"));
+
+    scrollbar.set_scroll_strategy(ScrollStrategy::StickToBottom);
 
     cursive.add_layer(LinearLayout::vertical()
         .child(BoxView::new(SizeConstraint::Fixed(10),
                             SizeConstraint::Full,
-                            Panel::new(TextView::new("")
-                                .with_id("output"))))
+                            Panel::new(scrollbar)))
         .child(text_box_view));
 
     // Starts the event loop.
