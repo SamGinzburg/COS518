98a33dccb20e39f4068139619a04ecb7a4ec0a70
SamGinzburg <ginzburg.sam@gmail.com>
2019-05-14 12:50:07 -0400

    WIP on vanity project

--- src/client/send.rs
+++ src/client/send.rs
@@ -7,6 +7,23 @@ use std::net::{IpAddr, SocketAddr};
 use std::string::String;
 use tarpc::{client, context};
 use tarpc_bincode_transport::connect;
+use crossbeam_channel::Sender;
+use cursive::CbFunc;
+use cursive::Cursive;
+use crate::receive_message;
+
+//
+// https://stackoverflow.com/questions/23975391/how-to-convert-a-string-into-a-static-str
+// we can intentionally leak the String, since we never want to delete our own conversation
+// history. 
+//
+// Supposedly there is a better way that utilizes the crossbeam crate, but I don't care, since
+// the project is finished, and this client is mostly a vanity project at this point since it
+// wasn't even used for the evaluation (testclient was).
+//
+fn string_to_static_str(s: String) -> &'static str {
+    Box::leak(s.into_boxed_str())
+}
 
 pub async fn rpc_put(
     server_addr: String,
@@ -14,6 +31,7 @@ pub async fn rpc_put(
     message: String,
     uid: usize,
     remote_uid: usize,
+    comm: Sender<Box<CbFunc>>,
 ) -> io::Result<()> {
     let server_addr = SocketAddr::new(IpAddr::V4(server_addr.parse().unwrap()), port);
     let transport = await!(connect(&server_addr)).unwrap();
@@ -45,20 +63,21 @@ pub async fn rpc_put(
     );
     // store the d_keys for when we receive a message at the end of the round
     // send it
-    println!("Encrypted msg: {:?}, len: {:?}", enc_msg, enc_msg.len());
+    //println!("Encrypted msg: {:?}, len: {:?}", enc_msg, enc_msg.len());
     let return_msg = await!(client.put(context::current(), enc_msg.clone())).unwrap();
-    println!("Response: {:?}, len: {:?}", return_msg, return_msg.len());
+    //println!("Response: {:?}, len: {:?}", return_msg, return_msg.len());
 
     if let Ok(unwrapped_msg) = unwrap(rn, return_msg.clone(), &pub_key, &dk, d_key.clone()) {
-        let output = String::from_utf8(unwrapped_msg).unwrap();
-        println!("{}\n", output);
+        let leaked_msg: &'static [u8] = Box::leak(unwrapped_msg.into_boxed_slice());
+        let mut output = String::from_utf8(leaked_msg.to_vec()).unwrap().clone();
+        output.push_str("\n");
+        let msg = string_to_static_str(output);
+        let _res = comm.send(Box::new(move |s: &mut Cursive| receive_message(s, msg)));
     } else if let Ok(_) = unwrap(rn, return_msg.clone(), &remote_pub_key, &dk, d_key) {
         println!("Received original message: remote not present.");
     } else {
         println!("Could not decrypt: system error.");
     }
-
-    //t_box.append(output.clone());
 
     Ok(())
 }
